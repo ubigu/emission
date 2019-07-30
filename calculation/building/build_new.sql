8edfd110c304cc261ae9d20c4504f26741d85c2a
Ilpo Tammi <ilpo@ubigu.fi>
2019-07-30 09:35:29 +0300

    Added material emissions

--- calculation/building/build_new.sql
+++ calculation/building/build_new.sql
@@ -1,15 +1,20 @@
-/*
-UUDISRAKENTAMINEN (energia)
-CONSTRUCTION OF NEW BUILDINGS (energy)
+/* Uudisrakentaminen (energia) | Construction of new buildings (energy)
 
-YKR-ruudun laskentavuoden uudisrakentamisen kasvihuonekaasupäästöt [t CO2-ekv/a] lasketaan seuraavasti:
-rak_uusi_energia_co2 = rakennus_ala * rak_energia_gco2m2 * muunto_massa
+YKR-ruudun laskentavuoden uudisrakentamisen energian kasvihuonekaasupäästöt [CO2-ekv/a] lasketaan seuraavasti:
+
+    rak_uusi_energia_co2 = rakennus_ala * rak_energia_gco2m2
+
+
+YKR-ruudun laskentavuoden uudisrakentamisen rakennustuotteiden kasvihuonekaasupäästöt rak_uusi_materia_co2 [CO2-ekv/a] lasketaan seuraavasti:
+ 
+    rak_uusi_materia_co2 = rakennus_ala * rak_materia_gco2m2
+
 
 */
 
 CREATE OR REPLACE FUNCTION
 public.il_build_new_co2(
-	rakennus_ala real, -- [m2] Rakennustyypin tietyn ikäluokan kerrosala YKR-ruudussa laskentavuonna. Lukuarvo riippuu laskentavuodesta ja rakennuksen tyypistä.
+	rakennus_ala real, -- Rakennustyypin tietyn ikäluokan kerrosala YKR-ruudussa laskentavuonna [m2]. Lukuarvo riippuu laskentavuodesta ja rakennuksen tyypistä.
     rakennusvuosi integer, -- Rakennusvuosikymmen tai -vuosi (2017 alkaen). Tässä: Rakennuksen valmistumisvuosi = laskentavuosi.
     year integer, -- Vuosi, jonka perusteella päästöt lasketaan / viitearvot haetaan
     rakennustyyppi varchar, -- Rakennustyyppi, esim. 'erpien', 'rivita'
@@ -18,7 +23,8 @@ public.il_build_new_co2(
 RETURNS real AS
 $$
 DECLARE
-    rak_energia_gco2m2 real; -- [gCO2-ekv/m2] Rakennustyypin rakentamisvaiheen työmaatoimintojen ja kuljetusten kasvihuonekaasujen ominaispäästöjä yhtä rakennettua kerrosneliötä kohti. Lukuarvo riippuu taustaskenaariosta, tarkasteluvuodesta ja rakennustyypistä.
+    rak_energia_gco2m2 real; -- Rakennustyypin rakentamisvaiheen työmaatoimintojen ja kuljetusten kasvihuonekaasujen ominaispäästöjä yhtä rakennettua kerrosneliötä kohti [gCO2-ekv/m2]. Arvo riippuu taustaskenaariosta, tarkasteluvuodesta ja rakennustyypistä.
+    rak_materia_gco2m2 real; -- Rakennustyypin rakentamiseen tarvittujen rakennustuotteiden tuotantoprosessin välillisiä kasvihuonekaasujen ominaispäästöjä yhtä rakennettua kerrosneliötä kohti [gCO2-ekv/m2]. Arvo riippuu taustaskenaariosta, tarkasteluvuodesta ja rakennustyypistä.
 BEGIN
 
 /* Palautetaan nolla, mikäli ruudun kerrosala on 0, -1 tai NULL */
@@ -41,9 +47,14 @@ ELSE
         EXECUTE 'SELECT ' || rakennustyyppi || ' FROM rakymp.rak_energia_gco2m2 WHERE vuosi = $1 AND skenaario = $2'
             INTO rak_energia_gco2m2  USING year, scenario;
         
+        /* Haetaan laskentavuoden ja kehitysskenaarion perusteella rakennustyyppikohtaiset uudisrakentamisen materiaalien valmistuksen kasvihuonekaasupäästöt */
+        /* Get the unit emissions for production of materials for construction by year of building, scenario and building type */
+        EXECUTE 'SELECT ' || CASE WHEN rakennustyyppi IN ('erpien', 'rivita', 'askert') THEN rakennustyyppi ELSE 'muut' END || ' FROM rakymp.rak_materia_gco2m2 WHERE vuosi = $1 AND skenaario = $2'
+            INTO rak_materia_gco2m2  USING year, scenario;
+
         /* Lasketaan ja palautetaan päästöt CO2-ekvivalenttia [gCO2-ekv/v] */
         /* Calculate and return emissions as CO2-equivalents [gCO2-ekv/a] */
-        RETURN rakennus_ala * rak_energia_gco2m2; -- rak_uusi_energia_co2
+        RETURN rakennus_ala * (rak_energia_gco2m2 + rak_materia_gco2m2); -- rak_uusi_energia_co2
 
     END IF;
 END IF;
