aa421059f06efe4f684eb2a3b44184a9e598a2dd
Ilpo Tammi <ilpo@ubigu.fi>
2019-08-27 09:32:28 +0300

    Altered data model etc

--- calculation/calculate_emissions_loop.sql
+++ calculation/calculate_emissions_loop.sql
@@ -17,38 +17,43 @@ public.il_calculate_emissions_loop(
 )
 RETURNS TABLE(
     xyind varchar(13),
-    yhteensa_tco2 real,
-    vesi_tco2 real,
-    lammitys_tco2 real,
-    jaahdytys_tco2 real,
-    kiinteistosahko_tco2 real,
+    tilat_vesi_tco2 real,
+    tilat_lammitys_tco2 real,
+    tilat_jaahdytys_tco2 real,
+    sahko_kiinteistot_tco2 real,
     sahko_kotitaloudet_tco2 real,
     sahko_palv_tco2 real,
     sahko_tv_tco2 real,
-    hloliikenne_tco2 real,
-    tvliikenne_tco2 real,
-    palvliikenne_tco2 real,
-    korjaussaneeraus_tco2 real,
-    purkaminen_tco2 real,
-    uudisrakentaminen_tco2 real,
-    vuosi smallint,
+    liikenne_hlo_tco2 real,
+    liikenne_tv_tco2 real,
+    liikenne_palv_tco2 real,
+    rak_korjaussaneeraus_tco2 real,
+    rak_purku_tco2 real,
+    rak_uudis_tco2 real,
+    sum_yhteensa_tco2 real,
+    sum_lammonsaato_tco2 real,
+    sum_liikenne_tco2 real,
+    sum_sahko_tco2 real,
+    sum_rakentaminen_tco2 real,
+    asukkaat int,
+    vuosi date,
     geom geometry(MultiPolygon, 3067))
 AS $$
 DECLARE
     laskentavuodet integer[];
-    vuosi integer;
+    calculationYear integer;
 BEGIN
 
     SELECT array(select generate_series(baseYear,targetYear)) INTO laskentavuodet;
 
-    FOREACH vuosi in ARRAY laskentavuodet
+    FOREACH calculationYear in ARRAY laskentavuodet
     LOOP
 
-        IF vuosi = baseYear THEN
+        IF calculationYear = baseYear THEN
             DROP TABLE IF EXISTS res;
-            CREATE TEMP TABLE res AS SELECT * FROM il_calculate_emissions(ykr_v, ykr_tp, ykr_rakennukset, aoi, vuosi, skenaario, metodi, sahkolaji, alue, baseYear, targetYear, kt_taulu, kv_taulu, jl_taulu);
+            CREATE TEMP TABLE res AS SELECT * FROM il_calculate_emissions(ykr_v, ykr_tp, ykr_rakennukset, aoi, calculationYear, skenaario, metodi, sahkolaji, alue, baseYear, targetYear, kt_taulu, kv_taulu, jl_taulu);
         ELSE 
-            INSERT INTO res SELECT * FROM il_calculate_emissions(ykr_v, ykr_tp, ykr_rakennukset, aoi, vuosi, skenaario, metodi, sahkolaji, alue, baseYear, targetYear, kt_taulu, kv_taulu, jl_taulu);
+            INSERT INTO res SELECT * FROM il_calculate_emissions(ykr_v, ykr_tp, ykr_rakennukset, aoi, calculationYear, skenaario, metodi, sahkolaji, alue, baseYear, targetYear, kt_taulu, kv_taulu, jl_taulu);
         END IF;
         
     END LOOP;
