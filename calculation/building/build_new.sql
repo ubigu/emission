1423bbaf2def21132c057e93d9ce7293e2c82581
Ilpo Tammi <ilpo@ubigu.fi>
2019-08-27 09:32:05 +0300

    Update build_new.sql

--- calculation/building/build_new.sql
+++ calculation/building/build_new.sql
@@ -11,12 +11,12 @@ YKR-ruudun laskentavuoden uudisrakentamisen rakennustuotteiden kasvihuonekaasup�
 
 
 */
-
+DROP FUNCTION IF EXISTS public.il_build_new_co2;
 CREATE OR REPLACE FUNCTION
 public.il_build_new_co2(
 	rakennus_ala real, -- Rakennustyypin tietyn ikäluokan kerrosala YKR-ruudussa laskentavuonna [m2]. Lukuarvo riippuu laskentavuodesta ja rakennuksen tyypistä.
-    rakennusvuosi integer, -- Rakennusvuosikymmen tai -vuosi (2017 alkaen). Tässä: Rakennuksen valmistumisvuosi = laskentavuosi.
-    year integer, -- Vuosi, jonka perusteella päästöt lasketaan / viitearvot haetaan
+  --  rakennusvuosi integer, -- Rakennusvuosikymmen tai -vuosi (2017 alkaen). Tässä: Rakennuksen valmistumisvuosi = laskentavuosi.
+    calculationYear integer, -- Vuosi, jonka perusteella päästöt lasketaan / viitearvot haetaan
     rakennustyyppi varchar, -- Rakennustyyppi, esim. 'erpien', 'rivita'
     scenario varchar -- PITKO:n mukainen kehitysskenaario
 )
@@ -37,26 +37,26 @@ ELSE
 
     /* Jos rakennusvuosi ei ole tarkasteltavana oleva vuosi, palauta 0 */
     /* If the year of construction is not the current year of calculation, return 0 */
-    IF rakennusvuosi != year THEN
-        RETURN 0;
+--    IF rakennusvuosi != year THEN
+--        RETURN 0;
     /* Muussa tapauksessa jatka laskentaan */
     /* In other cases, continue */
-    ELSE
+--    ELSE
         /* Haetaan laskentavuoden ja kehitysskenaarion perusteella rakennustyyppikohtaiset uudisrakentamisen energiankulutuksen kasvihuonekaasupäästöt */
         /* Get the unit emissions for energy consumption of construction by year of building, scenario and building type */
         EXECUTE 'SELECT ' || rakennustyyppi || ' FROM rakymp.rak_energia_gco2m2 WHERE vuosi = $1 AND skenaario = $2'
-            INTO rak_energia_gco2m2  USING year, scenario;
+            INTO rak_energia_gco2m2 USING calculationYear, scenario;
         
         /* Haetaan laskentavuoden ja kehitysskenaarion perusteella rakennustyyppikohtaiset uudisrakentamisen materiaalien valmistuksen kasvihuonekaasupäästöt */
         /* Get the unit emissions for production of materials for construction by year of building, scenario and building type */
         EXECUTE 'SELECT ' || CASE WHEN rakennustyyppi IN ('erpien', 'rivita', 'askert') THEN rakennustyyppi ELSE 'muut' END || ' FROM rakymp.rak_materia_gco2m2 WHERE vuosi = $1 AND skenaario = $2'
-            INTO rak_materia_gco2m2  USING year, scenario;
+            INTO rak_materia_gco2m2 USING calculationYear, scenario;
 
         /* Lasketaan ja palautetaan päästöt CO2-ekvivalenttia [gCO2-ekv/v] */
         /* Calculate and return emissions as CO2-equivalents [gCO2-ekv/a] */
         RETURN rakennus_ala * (rak_energia_gco2m2 + rak_materia_gco2m2); -- rak_uusi_energia_co2
 
-    END IF;
+--    END IF;
 END IF;
 
 END;
